ba6b855cc30915f2e150b2f9fbb2939c5f1cd10d
Ctkadlec <chase.kadlec@kellogg.northwestern.edu>
2020-03-15 19:13:45 +0000

    a meaningful commit message

--- app.rb
+++ app.rb
@@ -4,7 +4,8 @@ require "sinatra/reloader" if development?                                      
 require "sequel"                                                                      #
 require "logger"                                                                      #
 require "twilio-ruby"                                                                 #
-require "bcrypt"                                                                      #
+require "bcrypt"          
+                                                                                      #
 connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
 DB ||= Sequel.connect(connection_string)                                              #
 DB.loggers << Logger.new($stdout) unless DB.loggers.size > 0                          #
@@ -14,5 +15,71 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-events_table = DB.from(:events)
-rsvps_table = DB.from(:rsvps)
+restaurants_table = DB.from(:restaurants)
+reviews_table = DB.from(:reviews)
+users_table = DB.from(:users)
+
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end
+
+get "/" do
+    puts restaurants_table.all
+    @restaurants = restaurants_table.all.to_a
+    view "reviews"
+end
+
+get "/restaurants/:id" do
+    @restaurant = restaurants_table.where(id: params[:id]).to_a[0]
+    @reviews = reviews_table.where(restaurant_id: @restaurant[:id])
+    @users_table = users_table
+    view "review"
+end
+
+get "/restaurants/:id/reviews/new" do
+    @restaurant = restaurants_table.where(id: params[:id]).to_a[0]
+    view "new_review"
+end
+
+get "/restaurants/:id/reviews/create" do
+    puts params
+     @restaurant = restaurants_table.where(id: params["id"]).to_a[0]
+    reviews_table.insert(restaurant_id: params["id"],
+                       user_id: session["user_id"],
+                       comments: params["comments"])
+    view "create_review"
+end
+
+get "/users/new" do
+    view "new_user"
+end
+
+post "/users/create" do
+    puts params
+    hashed_password = BCrypt::Password.create(params["password"])
+    users_table.insert(name: params["name"], email: params["email"], password: hashed_password)
+    view "create_user"
+end
+
+get "/logins/new" do
+ view "new_login"
+
+end
+
+post "/logins/create" do
+    user = users_table.where(email: params["email"]).to_a[0]
+    puts BCrypt::Password::new(user[:password])
+    if user && BCrypt::Password::new(user[:password]) == params["password"]
+        session["user_id"] = user[:id]
+        @current_user = user
+        view "create_login"
+    else
+        view "create_login_failed"
+    end
+end
+
+get "/logout" do
+    session["user_id"] = nil
+    @current_user = nil
+    view "logout"
+end
--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,46 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :restaurants do
   primary_key :id
   String :title
   String :description, text: true
-  String :date
+  String :enjoyedon
   String :location
 end
-DB.create_table! :rsvps do
+DB.create_table! :reviews do
   primary_key :id
-  foreign_key :event_id
-  Boolean :going
+  foreign_key :restaurant_id
+  foreign_key :user_id
+  Boolean :checkin
   String :name
   String :email
   String :comments, text: true
 end
+DB.create_table! :users do
+  primary_key :id
+  String :name
+  String :email
+  String :password
+end
 
 # Insert initial (seed) data
-events_table = DB.from(:events)
+restaurants_table = DB.from(:restaurants)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+restaurants_table.insert(title: "Steamboat Barbecue", 
+                    description: "Local, hole in the wall spot for great barbecue!",
+                    enjoyedon: "June 21",
+                    location: "322 E Geneva Rd, Wheaton, IL")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+restaurants_table.insert(title: "Fire + Wine", 
+                    description: "Neopolitan pizza and Italian small plates!.",
+                    enjoyedon: "July 4",
+                    location: "433 N Main St, Glen Ellyn, IL")
+restaurants_table.insert(title: "The Burger Social", 
+                    description: "Very messy burgers, great tap list, and nice ambiance!.",
+                    enjoyedon: "July 4",
+                    location: "108 N Hale St, Wheaton, IL")   
+restaurants_table.insert(title: "Alfie's Inn", 
+                    description: "Comfort food, dated interior, but great option for takeout!.",
+                    enjoyedon: "July 4",
+                    location: "425 Roosevelt Rd, Glen Ellyn, IL")   
